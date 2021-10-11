459069cf9c2759a85f50a29ae132dfdf0cdd4bd8
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2021-10-11 21:22:36 +0200

    Refactored config

--- src/pdm_bump/config.py
+++ src/pdm_bump/config.py
@@ -1,6 +1,27 @@
 from typing import Optional, Any, cast
 from pdm.core import Project
 
+def _get_config_value(config: dict[str, Any], *keys: str) -> Optional[Any]:
+    while len(keys) > 1:
+        front: str = keys[0]
+        if front in config.keys():
+            config = cast(dict[str, Any], config[front])
+            keys = tuple(keys[1:])
+        else:
+            return None
+    
+    front: str = keys[0]
+    return None if front not in config.keys() else config[front]
+
+def _set_config_value(config: dict[str, Any], value: Any, *keys: str) -> None:
+    while len(keys) > 1:
+        front: str = keys[0]
+        if front not in config.keys():
+            config[front] = {}
+        keys = tuple(keys[1:])
+    
+    front: str = keys[0]
+    config[front] = value
 
 class Config:
     def __init__(self, project: Project) -> None:
@@ -8,25 +29,9 @@ class Config:
 
     def get_pyproject_value(self, *keys: str) -> Optional[Any]:
         config: dict[str, Any] = self.__project.pyproject
-        while len(keys) > 1:
-            front: str = keys[0]
-            if front in config.keys():
-                config = cast(dict[str, Any], config[front])
-                keys = tuple(keys[1:])
-            else:
-                return None
-        
-        front: str = keys[0]
-        return None if front not in config.keys() else config[front]
+        return _get_config_value(config, *keys)
 
     def set_pyproject_value(self, value: Any, *keys: str) -> None:
         config: dict[str, Any] = self.__project.pyproject
-        while len(keys) > 1:
-            front: str = keys[0]
-            if front not in config.keys():
-                config[front] = {}
-            keys = tuple(keys[1:])
-        
-        front: str = keys[0]
-        config[front] = value
+        _set_config_value(config, value, *keys)
             
