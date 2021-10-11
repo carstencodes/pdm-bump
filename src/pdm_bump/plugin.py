b493e6fe58f4a5d05e0a417cb622483dac2b1d98
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2021-10-11 20:27:58 +0200

    Added description to bump argument parser

--- src/pdm_bump/plugin.py
+++ src/pdm_bump/plugin.py
@@ -10,6 +10,7 @@ class BumpCommand(BaseCommand):
         parser.add_argument('what', action='store', choices=['major', 'minor', 'micro', 'pre-release', 'no-pre-release'], default=None, help="The part of the version to bump according to PEP 440: major.minor.micro.")
         parser.add_argument('--pre', action='store', choices=['alpha', 'beta', 'rc', 'c'], default=None, help="Sets a pre-release on the current version. If a pre-release is set, it can be removed using the final option. A new pre-release must greater then the current version. See PEP440 for details.")
         parser.add_argument("--dry-run", action='store_false', help='Do not perform a log-in')
+        parser.description = "Bumps the version to a next version according to PEP440."
 
     def handle(self, project: Project, options: Namespace) -> None:
         version_value: Optional[str] = None
