0e7c55c5e1ae809dd40079a2dcfb842249706b6f
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2022-08-21 00:43:25 +0200

    Added __version__ to package initialization

--- src/pdm_bump/__init__.py
+++ src/pdm_bump/__init__.py
@@ -1,7 +1,10 @@
 from typing import List
+from importlib.metadata import version as __get_version
 
 from .cli import main as register_plugin
 
 main = register_plugin
 
-__all__: List[str] = ["main"]
+__version__: str = __get_version(__package__ or __name__)
+
+__all__: List[str] = [ main.__name__ ]
