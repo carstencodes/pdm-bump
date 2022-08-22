fba7a78fe6965705c008ac3fd9b746aaf43a4d0b
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2022-08-22 22:11:26 +0200

    When running tests without pdm, no version is present

--- src/pdm_bump/__init__.py
+++ src/pdm_bump/__init__.py
@@ -1,10 +1,15 @@
-from typing import List
+from typing import List, Final
 from importlib.metadata import version as __get_version
+from importlib.metadata import PackageNotFoundError
 
 from .cli import main as register_plugin
 
 main = register_plugin
 
-__version__: str = __get_version(__package__ or __name__)
+try:
+    __version__: Final[str] = __get_version(__package__ or __name__)
+except PackageNotFoundError:
+    # Only occurs in development, since package is not installed properly
+    __version__: Final[str] = "0.0.0"
 
 __all__: List[str] = [main.__name__]
