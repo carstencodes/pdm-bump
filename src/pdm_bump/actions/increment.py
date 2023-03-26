c55d133c83c19d06479ba7a43977364f89ac4817
Carsten Igel <1760987+carstencodes@users.noreply.github.com>
2023-03-26 17:56:52 +0200

    feat: Added actions package
    In order to prepare more commands,
    move commands to separate package

---
+++ src/pdm_bump/actions/increment.py
@@ -0,0 +1,11 @@
+#
+# SPDX-License-Identifier: MIT
+#
+# Copyright (c) 2021-2023 Carsten Igel.
+#
+# This file is part of pdm-bump
+# (see https://github.com/carstencodes/pdm-bump).
+#
+# This file is published using the MIT license.
+# Refer to LICENSE for more information
+#
